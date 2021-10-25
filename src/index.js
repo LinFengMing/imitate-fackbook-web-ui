bc4e7e850f8b0a3dd5791484486cb79499eaab9e
jiro <jiro853141@gmail.com>
2021-10-25 14:51:54 +0800

    feat: 實戰仿Facebook UI，版面佈局

--- src/index.js
+++ src/index.js
@@ -1,18 +1,1 @@
 import "./index.css";
-
-const btn = document.getElementById('btn')
-const text = document.getElementById('text')
-// const box = document.getElementById('box')
-const boxUl = document.getElementById('boxUl')
-
-btn.addEventListener('click', function() {
-  text.classList.toggle('text-fb')
-  let str = ''
-  const li = `<li>test</li>`
-
-  for (let i = 0; i < 5; i++) {
-    str += li
-  }
-
-  boxUl.innerHTML = str
-})
